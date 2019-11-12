de9c09526d425fda3229deedb14c74bec82221c7
lespric <704640923@qq.com>
2019-11-12 12:41:13 +0800

    commit message

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
+/* GlobalObeject for nineBoxs {
+  boxsList ← 每个方盒的元素节点的数组
+  btnStart ← “开始闪”按钮的元素节点
+  btnEnd ← “结束闪”按钮的元素节点
+  bgColorChange ← 储存定时器的对象
+
+  colorRandom ← 颜色随机的函数方法 {
+    for i ← 0 to 5
+      do colorCode ← ((Math.random() * (n-m) + m) | 0 ) toString(16)
+         // 十六进制颜色的单个字符，取一位数并通过toString方法转为字符，而随机数中上式表示m~n的范围内
+         thisColor ← # + colorCode[0]~[5];
+      end
+
+    if thisColor != 原本颜色
+      then return thisColor
+      else colorRandom() //重新执行colorRandom()方法，直到与原本颜色不相等
+  }
+
+  boxRandom ← 位置随机的函数方法 {
+    a, b, c ← Math.floor(Math.random() * [m~n]) //再向下取舍，因floor的各个随机数概率差不多
+    if (a-b)*(a-c)*(b-c) != 0 //a不等于b，且a不等于c，且b不等于c，即三个随机数互不相等
+      then boxsList[a~c].style.backgroundColor ← colorRandom() //赋值给随机三个方盒随机的颜色
+      else boxRandom() //重新执行boxRandom()方法，直到互不相等
+  }
+
+  boxRecover ← 恢复原本颜色的函数方法 {
+    for i 0 to 8
+      do boxList[i].style.backgroundColor ← 原本颜色
+    end
+  }
+}
+// 定义一个专属于九宫格页面的全局对象
+
+btnStart.onclick ← 点击开始闪按钮触发的随机变色事件 {
+   btnStart.style.className ← 点击状态的CSS类，为了解耦
+   btnEnd.style.className ← 默认状态的CSS类
+
+   bgColorChange ← setInterval(
+     {
+     boxRecover() // 恢复原本颜色
+     boxRandom() // 位置随机且随机变色
+     }
+     1000ms // 添加个每1秒变化一次的定时器
+   )
+}
+btnEnd.onclick ← 点击结束闪按钮触发的取消变化事件 {
+   btnEnd.style.className ← 点击状态的CSS类，为了解耦
+   btnStart.style.className ← 默认状态的CSS类
+   
+   clearInterval(bgColorChange) // 在变化事件的对象删除定时器，即停止重复执行
+   boxRecover() // 恢复原本颜色
+}
+*/
+
 var nineBoxs = {
   boxsList: document.getElementById("nineBoxs").getElementsByClassName("box"),
   btnStart: document.getElementById("start"),
@@ -59,6 +113,7 @@ nineBoxs.btnStart.onclick = function() {
   }, 1000);
   // 设置间歇调用，先恢复原本颜色，再应用随机颜色，确保重复调用时保证只有三个随机颜色；
 };
+// 点击后，九宫格随机变色
 
 nineBoxs.btnEnd.onclick = function() {
   this.className = "btn-active";
@@ -66,7 +121,6 @@ nineBoxs.btnEnd.onclick = function() {
   // 点击后按钮变色
 
   clearInterval(nineBoxs.bgColorChange);
-  console.log(nineBoxs.bgColorChange);
   nineBoxs.boxRecover();
 };
 // 点击后取消间歇调用，并恢复原本颜色
